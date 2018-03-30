dc88734e5cf6972d2218bda9f028d6263fd39c75
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2018-03-30 13:42:19 -0700

    working app

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
+const giphyAPIKey = process.env.GIPHY_KEY
+const _ = require('lodash')
+const fetchGif = require('./utils/giphy.js')
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+module.exports = robot => {
+  robot.on('issue_comment.created', async context => {
+    // return if you ain't suppose be here
+    if (context.payload.comment.body.includes('gipht')) {
+      const searchTerm = _.sample([
+        'wat',
+        'pug',
+        'puppy',
+      ])
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+      const url = `http://api.giphy.com/v1/gifs/translate?api_key=${giphyAPIKey}&s=${searchTerm}`
+      const gifURL = await fetchGif(url)
+
+      const params = context.issue({body: `![](${gifURL})` })
+
+      return context.github.issues.createComment(params)
+
+    }
+  })
 }
